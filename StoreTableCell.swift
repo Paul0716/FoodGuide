519bdf30b631015df809e69d5e01efe37674e3ad
Paul Ku <justlove0714@hotmail.com>
2016-07-31 02:59:32 +0800

    customer table cell image present

--- StoreTableCell.swift
+++ StoreTableCell.swift
@@ -18,22 +18,30 @@ class StoreTableCell:UITableViewCell {
     
     func loadImage( photo: [AnyObject] ){
         
-        let previewPhoto = photo.first
+        if let previewPhoto = photo.first {
+            if let photo_ref = previewPhoto["photo_reference"],
+                height = previewPhoto["height"],
+                width = previewPhoto["height"] {
+                var parameters = [
+                    "key": "AIzaSyAmG59jZCzeqXlRK9wbytscseo5A0oxQ_8",
+                    "photoreference" : photo_ref!,
+                    "maxheight": height!,
+                    "maxwidth": width!
+                ]
+                
+                Alamofire.request( .GET, "https://maps.googleapis.com/maps/api/place/photo", parameters: parameters )
+                    .response {
+                        response in
+                        let (_,_,data,_) = response
+                        let img = UIImage(data: data!)
+                        self.PreviewImage.image = img
+                    }
+            }
+        }
         
-//        Alamofire
-//            .request(
-//                .GET,
-//                "https://maps.googleapis.com/maps/api/place/photo",
-//                parameters: [
-//                    "key": "AIzaSyAmG59jZCzeqXlRK9wbytscseo5A0oxQ_8",
-//                    "photoreference" : previewPhoto["photo_reference"],
-//                    "maxheight": previewPhoto["height"],
-//                    "maxwidth": previewPhoto["width"]
-//                ]
-//            )
-//            .response { response in
-//                
-//        }
+        
+        
+
         
     }
 }
